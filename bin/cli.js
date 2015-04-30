2b47def59e5e75bd9efe13478901854b8b29fdda
yieme <yieme@cormea.com>
2015-04-29 19:40:52 -0600

    add cli

---
+++ bin/cli.js
@@ -0,0 +1,13 @@
+#!/usr/local/bin/node
+
+'use strict';
+
+var stdin = require('get-stdin');
+var parse = require('../index.js')
+
+stdin(function (data) {
+  if (data) {
+    var result = parse(data)
+    console.log(result);
+  }
+})
